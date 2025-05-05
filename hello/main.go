e3220add5424c7db369c9f11f28b85680759303d
kate keiroz <katekeiroz@gmail.com>
2025-05-05 19:20:14 +0100

    http, API

--- hello/main.go
+++ hello/main.go
@@ -1,21 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
 	fmt.Println("---List of my to-do ---")
-	var meeting = "meeting on Tuesday"
-	var appoint = "doctor on Friday"
-	var party = "Leah's birthday on Saturday"
 
-	var taskItems = []string{meeting, appoint, party} //chama-se slice, pois nao eh limitado os values dentro. se por acaso eu limito ex [2] seria uma array
-	printTasks(taskItems)
+	http.HandleFunc("/hello-go", helloUser)
+
+	http.ListenAndServe(":8080", nil)
 
 }
 
-func printTasks(taskItems []string) //passando parametros e especificando o tipo {
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+	var greeting = "hello user. Welcome to our to do list App"
+	fmt.Fprintln(writer, greeting)
+
+}
+
+func printTasks(taskItems []string) { //passando parametros e especificando o tipo {
 	for index, tasks := range taskItems {
 		fmt.Printf("%d. %s\n", index+1, tasks) // placeholders na string"" e na sequencia os values correspondentes...
 	}
+}
+
+func addTask(taskItems []string, newTask string) {
+	var updatedTask = append(taskItems, newTask) // append juntar
+	printTasks(updatedTask)
 }
